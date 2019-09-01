2b4ebaad3b4964880cb8ad6883fd5a198e9482b3
Robert Sim <rsim@microsoft.com>
2019-08-31 20:23:45 -0700

    First import

---
+++ processCatalog.py
@@ -0,0 +1,168 @@
+import json
+
+import pprint
+import sys
+import os
+import subprocess
+
+import spotipy
+import spotipy.util as util
+import datetime
+import requests
+
+# Sample config:
+# Visit https://developer.spotify.com/dashboard/applications to create a clientid.
+# Be sure to white-list a redirect URI (Eg https://localhost:8080)
+# The redirect URI doesn't have to be functional but you will need to copy it 
+# from your browser when prompted.
+# {
+#    'environment': {
+#       'SPOTIPY_CLIENT_ID': <your spotify app id>,
+#       'SPOTIPY_CLIENT_SECRET': <the secret for your spotify app id>
+#       'SPOTIPY_REDIRECT_URI': 'https://localhost:8080'
+#    },
+#
+# }    
+
+def setEnvironment(config):
+    if 'environment' in config:
+        for e in config['environment']:
+            os.environ[e]=config['environment'][e]
+
+def formatDate(date):
+    return date.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+def makeUrl(startdate, enddate):
+    return '{}/play?start_time={}&end_time={}'.format(
+        'https://legacy-api.kexp.org',
+        formatDate(startdate), formatDate(enddate))
+
+def fetchDate(start, end):
+    cacheFn = 'cache/{}.json'.format(start.strftime("%Y%m%d"))
+    if not os.path.exists(cacheFn):        
+        nextUrl = makeUrl(start, end)
+        cachetracks=[]
+        print("Fetching {}".format(start.strftime("%Y-%m-%d")))            
+        while nextUrl is not None:
+            print('\t'+nextUrl.replace('https://legacy-api.kexp.org',''))
+            result=requests.get(nextUrl)
+            if result.ok:
+                response = result.json()
+                nextUrl = response['next']
+                tracks = response['results']
+                print(len(tracks))
+                for t in tracks:
+                    #print(json.dumps(t))
+                    if 'track' in t:
+                        cachetracks.append(t)
+            else:
+                print(json.dumps(result))
+                nextUrl=None
+        with open(cacheFn,'w', encoding='utf-8') as outCache:
+            outCache.write(json.dumps(cachetracks))
+    tracks=[]
+    print("Loading {}".format(cacheFn))
+    with open(cacheFn, 'r', encoding='utf-8') as inCache:
+        tracks = json.load(inCache)
+    print(len(tracks))
+    return tracks
+
+
+
+def collectFromKEXP(config):
+    daysToParse= config['daysToParse'] if 'daysToParse' in config else 7
+
+    today = datetime.datetime.utcnow().date()
+    start_date = today - datetime.timedelta(days=daysToParse)
+    end_date = start_date + datetime.timedelta(days=1)
+    results = []
+    while (start_date<today):
+        results += fetchDate(start_date,end_date)
+        start_date = start_date + datetime.timedelta(days=1)
+        end_date = start_date + datetime.timedelta(days=1)
+    print("All results: ",len(results))
+    return results  
+    
+
+def updateSpotify(config, catalog):
+    result={}
+
+    # we rank artists by most plays, take the top 25 and 
+    # add their played tracks to the playlist.
+    for r in catalog:
+        if r['track'] is None:
+            continue
+        artistid = r['artist']['name']
+        if artistid not in result:
+            result[artistid]={'track':r, 'plays':set(), 'songs':set()}
+        result[artistid]['plays'].add(r['airdate']) # count by unique timestamps. Sometimes the playlist has duplicates.
+        result[artistid]['songs'].add(r['track']['name'])
+    
+    print(len(result['Portishead']['plays']))
+    username = config['spotify_username']
+    playlist_name= config['playlist_name']
+    
+    # If you don't have a cached token this will trigger a web flow.
+    scope = 'playlist-modify-public playlist-modify-private'
+    token = util.prompt_for_user_token(username, scope)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        #sp.trace = True
+        #sp.trace_out = True
+        #print(dir(sp))
+        # It's better to create this manually.
+        #playlists = sp.user_playlist_create(username, playlist_name, description=playlist_description, public=True)
+        #                                    #playlist_description)    
+        #pprint.pprint(playlists)
+        playlists=sp.user_playlists(username)
+        
+        pl_id=None
+        for r in playlists['items']:            
+            if r['name']==playlist_name:
+                pl_id=r['id']
+        if pl_id is None:
+            raise Exception("Can't find playlist {}".format(config['playlist_name']))
+
+        track_ids = []
+        for r in sorted(result, key=lambda x: len(result[x]['plays']), reverse=True)[0:25]:
+            print(r,len(result[r]['plays']))
+            for s in result[r]['songs']:
+                print('\t{}'.format(s))            
+                query = "artist:{} track:{}".format(r,s)
+                try:
+                    searchResult=sp.search(query)
+                    if (len(searchResult['tracks']['items'])>0):
+                        #pprint.pprint(searchResult['tracks']['items'][0])
+                        track_ids.append(searchResult['tracks']['items'][0]['id'])
+                    else:
+                        print("No search result for {}".format(query))
+                        # try a keyword search instead
+                        query= "{} {}".format(r,s)
+                        searchResult=sp.search(query)
+                        if (len(searchResult['tracks']['items'])>0):
+                            track_ids.append(searchResult['tracks']['items'][0]['id'])
+                        else:
+                            print("No fallback result either.")
+                except:
+                    print("Failed: {}".format(query))    
+        
+
+        print(json.dumps(track_ids))
+
+        sp.user_playlist_replace_tracks(username, pl_id, track_ids)
+        playlist_description ='Top 25 artists on KEXP as of {}'.format(datetime.datetime.now().strftime('%x'))
+        #sp.user_playlist_change_details(username, pl_id, playlist_description)
+    else:
+        print("Can't get token for", username)
+
+def main():
+    with open('config.json', 'r', encoding='utf-8') as inCfg:
+        config=json.load(inCfg)
+    
+    setEnvironment(config)
+    catalog=collectFromKEXP(config)
+    updateSpotify(config, catalog)
+
+if __name__ == "__main__":
+    main()
